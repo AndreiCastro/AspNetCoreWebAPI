f0143000dba01c198a6ac931ea99b18e64fe47c9
AndreiCastro <andrei.castro1991@gmail.com>
2021-04-19 19:22:21 -0300

    Incluindo pasta DATA

---
+++ SmartSchool/Data/SmartContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmartSchool.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartSchool.Data
+{
+    public class SmartContext : DbContext
+    {
+        public SmartContext(DbContextOptions<SmartContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Aluno> Alunos { get; set; }
+        public DbSet<Disciplina> Disciplinas { get; set; }
+        public DbSet<AlunoDisciplina> AlunoDisciplinas { get; set; }
+        public DbSet<Professor> Professores { get; set; }
+
+        //Relação de N pra N
+        protected override void OnModeCreating(ModelBuilder builder)
+        {
+            //dizendo que o AlunoDisciplina tem id das tabelas Aluno e Disciplina (olhando a classe fica mais compreensível)
+            builder.Entity<AlunoDisciplina>()
+                .HasKey(AD => new { AD.AlunoId, AD.DisciplinaId });
+        }
+    }
+
+}
