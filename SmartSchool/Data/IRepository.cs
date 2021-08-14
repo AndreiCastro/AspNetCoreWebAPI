87e02e61d1a205e8628634a798168aea9b8889ff
AndreiCastro <andrei.castro1991@gmail.com>
2021-08-14 14:34:08 -0300

    Incluindo n=NewtonsoftJson no csproj

---
+++ SmartSchool/Data/IRepository.cs
@@ -0,0 +1,26 @@
+﻿using SmartSchool.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartSchool.Data
+{
+   public  interface IRepository
+    {
+        void Add<T>(T entity) where T : class; //Isso significa: Que o add espera uma class, e usa essa classe como parametro
+        void Update<T>(T entity) where T : class;
+        void Delete<T>(T entity) where T : class;
+        bool SaveChanges();
+
+        Aluno[] GetAllAlunos(bool includeProfessor = false);
+        Aluno[] GetAllAlunoByDisciplinaId(int idDisciplina, bool includeProfessor = false);
+        Aluno GetAlunoById(int idAluno, bool includeProfessor = false);
+
+        Professor[] GetAllProfessores(bool includeAluno = false);
+        Professor[] GetAllProfessoreByDisciplinaId(int idDisciplina, bool includeAluno = false);
+        Professor GetProfessorById(int idProfessor, bool includeAluno = false);
+
+
+    }
+}
---
+++ SmartSchool/Data/Repository.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmartSchool.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartSchool.Data
+{
+    public class Repository : IRepository
+    {
+        private readonly SmartContext _context;
+
+        public Repository(SmartContext context)
+        {
+            _context = context;
+        }
+        public void Add<T>(T entity) where T : class
+        {
+            _context.Add(entity);
+        }
+
+        public void Update<T>(T entity) where T : class
+        {
+            _context.Update(entity);
+        }
+
+        public void Delete<T>(T entity) where T : class
+        {
+            _context.Remove(entity);
+        }
+
+        public bool SaveChanges()
+        {
+            return _context.SaveChanges() > 0;
+        }
+
+        //Aluno
+        public Aluno[] GetAllAlunos(bool includeProfessor = false)
+        {
+            IQueryable<Aluno> query = _context.Alunos;
+
+            if (includeProfessor)
+            {
+                query = query.Include(ad => ad.AlunosDisciplinas)
+                             .ThenInclude(d => d.Disciplina)
+                             .ThenInclude(p => p.Professor);
+            }
+
+            //AsNoTracking serve para liberar a sessão após consulta, para atualização
+            query = query.AsNoTracking().OrderBy(x => x.Id);
+            return query.ToArray();
+        }
+
+        public Aluno[] GetAllAlunoByDisciplinaId(int idDisciplina, bool includeProfessor = false)
+        {
+            IQueryable<Aluno> query = _context.Alunos;
+
+            if(includeProfessor)
+            {
+                query = query.Include(ad => ad.AlunosDisciplinas)
+                             .ThenInclude(d => d.Disciplina)
+                             .ThenInclude(p => p.Professor);
+            }
+
+            query = query.AsNoTracking().OrderBy(x => x.Id)
+                                        .Where(y => y.AlunosDisciplinas.Any(d => d.DisciplinaId == idDisciplina));
+            return query.ToArray();
+        }
+
+        public Aluno GetAlunoById(int idAluno, bool includeProfessor = false)
+        {
+            IQueryable<Aluno> query = _context.Alunos;
+
+            if(includeProfessor)
+            {
+                query = query.Include(ad => ad.AlunosDisciplinas)
+                             .ThenInclude(d => d.Disciplina)
+                             .ThenInclude(p => p.Professor);
+            }
+
+            query = query.AsNoTracking().Where(x => x.Id == idAluno);
+            return query.FirstOrDefault();
+        }
+
+        //Professor
+        public Professor[] GetAllProfessores(bool includeAluno = false)
+        {
+            IQueryable<Professor> query = _context.Professores;
+
+            if(includeAluno)
+            {
+                query = query.Include(d => d.Disciplinas)
+                             .ThenInclude(ad => ad.AlunosDisciplinas)
+                             .ThenInclude(a => a.Aluno);
+            }
+
+            query = query.AsNoTracking().OrderBy(x => x.Id);
+            return query.ToArray();
+        }
+
+        public Professor[] GetAllProfessoreByDisciplinaId(int idDisciplina, bool includeAluno = false)
+        {
+            IQueryable<Professor> query = _context.Professores;
+
+            if(includeAluno)
+            {
+                query = query.Include(d => d.Disciplinas)
+                             .ThenInclude(ad => ad.AlunosDisciplinas)
+                             .ThenInclude(a => a.Aluno);
+            }
+
+            query = query.AsNoTracking().Where(x => x.Disciplinas.Any(y => y.AlunosDisciplinas
+                                                                 .Any(z => z.DisciplinaId == idDisciplina)))
+                                                                 .OrderBy(x => x.Id);
+            return query.ToArray();
+        }
+
+        public Professor GetProfessorById(int idProfessor, bool includeAluno = false)
+        {
+            IQueryable<Professor> query = _context.Professores;
+
+            if(includeAluno)
+            {
+                query = query.Include(d => d.Disciplinas)
+                             .ThenInclude(ad => ad.AlunosDisciplinas)
+                             .ThenInclude(a => a.Aluno);
+            }
+
+            query = query.AsNoTracking().Where(x => x.Id == idProfessor);
+            return query.FirstOrDefault();
+        }
+    }
+}
